d45b05de5a4e0c9d40a684fbc032e07eaf2a55f9
Nomi <nomi.car@gmail.com>
2019-03-06 15:12:59 +0000

    Update debriefForm.js

--- src/debriefForm.js
+++ src/debriefForm.js
@@ -13,21 +13,10 @@ export default function drawDebriefForm() {
     let header = div.appendChild(document.createElement('h1'));
     header.id = 'debriefTitle';
     div.className = 'debrief';
-    header.innerText = 'finally...';
+    header.innerText = 'Thank you for participating!';
     let form = div.appendChild(document.createElement('form'));
     form.id = 'debriefForm';
     form.className = 'debrief';
-    let manipulation = form.appendChild(document.createElement('div'));
-    manipulation.id = 'debriefManipulationContainer';
-    manipulation.className = 'debrief';
-    let manipulationQ = manipulation.appendChild(document.createElement('div'));
-    manipulationQ.id = 'debriefManipulationQuestion';
-    manipulationQ.className = 'debrief';
-    manipulationQ.innerHTML = 'The advisors differed from one another in their advice-giving. ' +
-        'What do you think the difference between the advisors was?';
-    let manipulationA = manipulation.appendChild(document.createElement('textarea'));
-    manipulationA.id = 'debriefManipulationAnswer';
-    manipulationA.className = 'debrief';
     let comment = form.appendChild(document.createElement('div'));
     comment.id = 'debriefCommentContainer';
     comment.className = 'debrief';
